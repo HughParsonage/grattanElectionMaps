ffa2ab66e1c933007cc95d5a4cb240f1510edd2c
HughParsonage <hugh.parsonage@gmail.com>
2016-09-11 14:49:58 +1000

    initial commit of print_city_plot

---
+++ R/print_city_plot.R
@@ -0,0 +1,14 @@
+#' Print inset of city
+#'
+#' @param city A city code.
+#' @param xpos, ypos The positions in \code{npc} that the inset is the be drawn.
+#' @param vfactor By how much should the city inset be scaled \strong{relative to other cities}.
+
+print_city_plot <- function(city, xpos, ypos, vfactor){
+  pr <- plot_region(city)
+  vp <- grid::viewport(width = pr$vwidth * vfactor,
+                       height = pr$vheight * vfactor ,
+                       x = xpos,
+                       y = ypos)
+  print(pr$p, newpage = FALSE, vp = vp)
+}
