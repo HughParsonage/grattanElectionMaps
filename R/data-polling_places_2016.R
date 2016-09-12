17c7649b38f05c8ca203442f124e0ff785e03871
HughParsonage <hugh.parsonage@gmail.com>
2016-09-12 15:42:54 +1000

    bare-bones documentation for polling places

--- R/data-polling_places_2016.R
+++ R/data-polling_places_2016.R
@@ -1,3 +1,21 @@
-#' Polling place information for 2016
+#' Polling place information for 2004 to 2016
+#' @source \url{http://aec.gov.au}
+#' @name polling_places_YEAR
+NULL
 
+#' @rdname polling_places_YEAR
 "polling_places_2016"
+
+#' @rdname polling_places_YEAR
+"polling_places_2013"
+
+#' @rdname polling_places_YEAR
+"polling_places_2010"
+
+#' @rdname polling_places_YEAR
+"polling_places_2007"
+
+#' @rdname polling_places_YEAR
+"polling_places_2004"
+
+
