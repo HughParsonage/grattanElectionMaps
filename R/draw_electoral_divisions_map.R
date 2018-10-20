3e5edef9c6dbb4cd49c8e42d30e2df49d6bead70
HughParsonage <hugh.parsonage@gmail.com>
2018-10-20 16:44:37 +1100

    Update for latest ggplot2, scale_gradient

--- R/draw_electoral_divisions_map.R
+++ R/draw_electoral_divisions_map.R
@@ -3,51 +3,124 @@
 #' @param .polygonal.data Data including the vertices of all polygons, including a \code{fill} variable to colour the polygons.
 #' @param year What elections' boundaries should be used?
 #' @param scale_fill_manual_args A list of arguments to pass to \code{scale_fill_manual}.
+#' @param scale_fill_gradientn_args A list of arguments to pass to \code{scale_fill_gradientn}.
 #' @param polygon_outline_colour The outline of polygons.
 #' @param city_inset_scale By how much should the cities' insets be scaled by relative to the Australia plot?
+#' @param base_size,base_family Passed to \code{theme_map}.
+#' @param city_inset_text_size Title size in points.
 #' @param legend.title What should the legend title (for \code{fill}) be?
 #' @return A printed ggplot object.
+#' 
+#' @examples
+#' library(grattanCharts)
+#' library(hutils)
+#' library(ggplot2)
+#' library(scales)
+#' library(data.table)
+#' library(magrittr)
+#' system.file(package = "grattanElectionMaps",
+#'            "examples/wellbeing.csv") %>%
+#'  fread %>%
+#'  .[, ELECT_DIV := toupper(gsub("[^A-Za-z]", "", Electorate))] %>%
+#'  .[aus_division_elect_map_simple_2016, on = "ELECT_DIV"] %T>%
+#'  .[, stopifnot(!anyNA(SE), uniqueN(Rank) == 150L)] %>%
+#'  setnames("Personal wellbeing index", "fill") %>%
+#'  draw_electoral_divisions_map(
+#'    scale_fill_gradientn_args = list(colors = gpal(7)),
+#'    legend.title = "Well-being",
+#'    city_inset_scale = c("PER" = 26,
+#'                         "ADL" = 24,
+#'                         "MEL" = 18,
+#'                         "SYD" = 14,
+#'                         "BNE" = 14),
+#'    city_inset_text_size = 18, 
+#'    base_size = 20)
+#' 
+#' 
+#' 
 #' @export
 #'
 
 draw_electoral_divisions_map <- function(.polygonal.data,
                                          year = c("2016", "2013", "2010", "2007", "2004"),
                                          scale_fill_manual_args = NULL,
+                                         scale_fill_gradientn_args = NULL,
                                          polygon_outline_colour = "black",
-                                         city_inset_scale = 12,
+                                         city_inset_scale = 13,
+                                         base_size = 25,
+                                         base_family = "",
+                                         city_inset_text_size = 25,
                                          legend.title = NULL){
-
+  
   stopifnot(all(c("lat", "long", "fill", "group") %in% names(.polygonal.data)))
-
+  
   year <- match.arg(year)
-
+  
   entire <-
     ggplot2::ggplot(.polygonal.data) +
     ggplot2::geom_polygon(ggplot2::aes_string(x = "long", y = "lat", fill = "fill", group = "group"),
                           color = polygon_outline_colour) +
     ggplot2::coord_map(xlim = aus_extent$xlim,
                        ylim = aus_extent$ylim) +
-    ggthemes::theme_map(base_size = 25)
-
+    ggthemes::theme_map(base_size = base_size,
+                        base_family = base_family)
+  
   if (!is.null(scale_fill_manual_args)){
     entire <- entire + do.call(ggplot2::scale_fill_manual, args = scale_fill_manual_args)
   }
-
+  if (!is.null(scale_fill_gradientn_args)){
+    entire <- entire + do.call(ggplot2::scale_fill_gradientn, args = scale_fill_gradientn_args)
+  }
+  
   if (is.null(legend.title)){
     entire <- entire + ggplot2::theme(legend.title = ggplot2::element_blank())
   } else {
     entire <- entire + ggplot2::guides(fill = ggplot2::guide_legend(title = legend.title))
   }
-
+  
   entire <- entire + ggplot2::theme(legend.position = c(0, 1),
                                     legend.justification = c(0, 1),
-                                    legend.margin = grid::unit(0, "lines"))
-
+                                    legend.margin = ggplot2::margin())
+  
+  if (length(city_inset_scale) == 1L) {
+    city_inset_scale <- 
+      c("PER" = city_inset_scale,
+        "ADL" = city_inset_scale,
+        "MEL" = city_inset_scale,
+        "SYD" = city_inset_scale,
+        "BNE" = city_inset_scale)
+  }
   print(entire, newpage = TRUE)
-
-  print_city_plot(.polygonal.data, "PER", xpos = 0.100, ypos = 0.450, vfactor = 1, scale_fill_manual_args = scale_fill_manual_args, city_inset_scale = city_inset_scale)
-  print_city_plot(.polygonal.data, "ADL", xpos = 0.380, ypos = 0.325, vfactor = 1, scale_fill_manual_args = scale_fill_manual_args, city_inset_scale = city_inset_scale)
-  print_city_plot(.polygonal.data, "MEL", xpos = 0.525, ypos = 0.130, vfactor = 1, scale_fill_manual_args = scale_fill_manual_args, city_inset_scale = city_inset_scale)
-  print_city_plot(.polygonal.data, "SYD", xpos = 0.845, ypos = 0.200, vfactor = 1, scale_fill_manual_args = scale_fill_manual_args, city_inset_scale = city_inset_scale)
-  print_city_plot(.polygonal.data, "BNE", xpos = 0.875, ypos = 0.725, vfactor = 1, scale_fill_manual_args = scale_fill_manual_args, city_inset_scale = city_inset_scale)
+  
+  .print_city_plot <- function(...) {
+    print_city_plot(...,
+                    title.size = city_inset_text_size,
+                    base_family = base_family)
+  }
+  
+  .print_city_plot(.polygonal.data, "PER", xpos = 0.100, ypos = 0.450,
+                  vfactor = 1, 
+                  scale_fill_manual_args = scale_fill_manual_args,
+                  scale_fill_gradientn = scale_fill_gradientn_args,
+                  city_inset_scale = city_inset_scale["PER"])
+  .print_city_plot(.polygonal.data, "ADL", xpos = 0.380, ypos = 0.325,
+                  vfactor = 1, 
+                  scale_fill_manual_args = scale_fill_manual_args,
+                  scale_fill_gradientn = scale_fill_gradientn_args,
+                  city_inset_scale = city_inset_scale["ADL"])
+  .print_city_plot(.polygonal.data, "MEL", xpos = 0.525, ypos = 0.130,
+                  vfactor = 1, 
+                  scale_fill_manual_args = scale_fill_manual_args,
+                  scale_fill_gradientn = scale_fill_gradientn_args,
+                  city_inset_scale = city_inset_scale["MEL"])
+  .print_city_plot(.polygonal.data, "SYD", xpos = 0.845, ypos = 0.200,
+                  vfactor = 1, 
+                  scale_fill_manual_args = scale_fill_manual_args,
+                  scale_fill_gradientn = scale_fill_gradientn_args,
+                  city_inset_scale = city_inset_scale["SYD"])
+  .print_city_plot(.polygonal.data, "BNE", xpos = 0.875, ypos = 0.725,
+                  vfactor = 1, 
+                  scale_fill_manual_args = scale_fill_manual_args,
+                  scale_fill_gradientn = scale_fill_gradientn_args,
+                  city_inset_scale = city_inset_scale["BNE"])
 }
--- R/plot_region.R
+++ R/plot_region.R
@@ -3,10 +3,13 @@
 #' @param .polygonal.data Polygonal data (of Australia). See \link{draw_electoral_divisions_map}.
 #' @param city Which city to plot
 #' @param coord_lines Plot grid lines at 0.1 intervals.
-#' @param scale_fill_manual_args A list of arguments to pass to \code{scale_fill_manual}.
+#' @param scale_fill_manual_args,scale_fill_gradientn_args A \code{list} of arguments
+#'  to pass to \code{scale_fill_manual} or \code{scale_fill_gradientn}.
 #' @param polygon_outline_colour The outline of polygons.
 #' @param city_inset_scale Scale relative to Australia.
 #' @param title (logical) Should a standard \code{ggtitle} be added to each city?
+#' @param title.size The base size of the plot, in points.
+#' @param base_size,base_family Passed to \code{theme_map}.
 #' @importFrom magrittr %>%
 #' @import data.table
 #' @return A list of three elements, \code{vwidth} the width of the inset on the device, \code{vheight} the height of the inset on the device, and \code{p} the plot.
@@ -16,9 +19,13 @@ plot_region <- function(.polygonal.data,
                         city = c("PER", "ADL", "MEL", "SYD", "BNE"),
                         coord_lines = c("none", "latlon", "lat", "lon"),
                         scale_fill_manual_args = NULL,
+                        scale_fill_gradientn_args = NULL,
                         polygon_outline_colour = "black",
                         city_inset_scale = 15,
-                        title = TRUE){
+                        title = TRUE, 
+                        base_size = 25,
+                        base_family = "",
+                        title.size = 25) {
   city <- match.arg(city)
   coord_lines <- match.arg(coord_lines)
 
@@ -38,8 +45,8 @@ plot_region <- function(.polygonal.data,
     # (When plotting Division of Sydney or Division of Fraser/Fenner)
     .[long < 154] %>%
     .[, include.it := all(data.table::between(long, xlim1, xlim2),
-                          data.table::between(lat , ylim1, ylim2)), by = "ELECT_DIV"] %>%
-    dplyr::ungroup(.) %>%
+                          data.table::between(lat , ylim1, ylim2)),
+      by = "ELECT_DIV"] %>%
     as.data.table(.) %>%
     .[as.logical(include.it)]
 
@@ -53,7 +60,7 @@ plot_region <- function(.polygonal.data,
   xlength <- diff(xlim)
   ylength <- diff(ylim)
 
-  xlim <- c(xlim[1] - 0.025 * xlength,
+  xlim <- c(xlim[1] - 0.025 * xlength - (city == "ADL") * xlength,
             xlim[2] + 0.025 * xlength)
   ylim <- c(ylim[1] - 0.025 * ylength,
             ylim[2] + 0.025 * ylength)
@@ -61,7 +68,10 @@ plot_region <- function(.polygonal.data,
   pp <-
     the_divisions_to_plot %>%
     ggplot2::ggplot(.) +
-    ggplot2::geom_polygon(ggplot2::aes_string(x = "long", y = "lat", fill = "fill", group = "group"),
+    ggplot2::geom_polygon(ggplot2::aes_string(x = "long", 
+                                              y = "lat", 
+                                              fill = "fill",
+                                              group = "group"),
                           color = polygon_outline_colour)
 
   if (coord_lines != "none"){
@@ -93,18 +103,48 @@ plot_region <- function(.polygonal.data,
   pp <-
     pp +
     ggplot2::coord_map(xlim = xlim, ylim = ylim) +
-    ggthemes::theme_map(base_size = 25) +
-    ggplot2::theme(legend.position = "none")
+    ggthemes::theme_map(base_size = base_size,
+                        base_family = base_family) +
+    ggplot2::theme(legend.position = "none",
+                   title = ggplot2::element_text(size = title.size),
+                   plot.margin = ggplot2::margin())
 
   if (!is.null(scale_fill_manual_args)){
-    pp <- pp + do.call(ggplot2::scale_fill_manual, args = scale_fill_manual_args)
+    pp <- pp + do.call(ggplot2::scale_fill_manual,
+                       args = scale_fill_manual_args)
+  }
+  if (!is.null(scale_fill_gradientn_args)){
+    pp <- pp + do.call(ggplot2::scale_fill_gradientn, 
+                       args = scale_fill_gradientn_args)
   }
 
   if (title){
     the_title <- cities_abbrev[[city]]
-    pp <- pp + ggplot2::ggtitle(the_title)
+    switch (city,
+            "ADL" = {
+              pp <- pp + ggplot2::annotate(geom = "text",
+                                           label = "Adelaide",
+                                           family = base_family,
+                                           x = xlim[1],
+                                           y = mean(ylim),
+                                           size = 1.2*title.size / (14/5),
+                                           hjust = 0)
+            },
+            "MEL" = {
+              pp <-
+                pp +
+                ggplot2::ggtitle(the_title) +
+                ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5))
+            },
+            "PER" = {
+              pp <-
+                pp +
+                ggplot2::ggtitle(the_title) +
+                ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5))
+            },
+            pp <- pp + ggplot2::ggtitle(the_title))
   }
-
+  
   list(
     vwidth = city_inset_scale * xlength / lapply(aus_extent, diff)$xlim,
     vheight = city_inset_scale * ylength / lapply(aus_extent, diff)$ylim,
